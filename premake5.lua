3256413301f8fd42bbb65eb283d473073e5700e3
ThirteenAG <thirteenag@gmail.com>
2017-03-18 16:51:20 +0300

    fps limit

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+workspace "d3d9-wrapper"
+   configurations { "Release", "Debug" }
+   platforms { "Win32" }
+   architecture "x32"
+   location "build"
+   objdir ("build/obj")
+   buildlog ("build/log/%{prj.name}.log")
+   
+   kind "SharedLib"
+   language "C++"
+   targetdir "data"
+   targetname "d3d9"
+   targetextension ".dll"
+   characterset ("MBCS")
+   flags { "StaticRuntime" }
+   
+   defines { "rsc_CompanyName=\"ThirteenAG\"" }
+   defines { "rsc_LegalCopyright=\"MIT License\""} 
+   defines { "rsc_FileVersion=\"1.0.0.0\"", "rsc_ProductVersion=\"1.0.0.0\"" }
+   defines { "rsc_InternalName=\"%{prj.name}\"", "rsc_ProductName=\"%{prj.name}\"", "rsc_OriginalFilename=\"%{prj.name}.asi\"" }
+   defines { "rsc_FileDescription=\"https://thirteenag.github.io/wfp\"" }
+   defines { "rsc_UpdateUrl=\"https://github.com/ThirteenAG/%{prj.name}\"" }
+   
+   files { "source/*.h", "source/*.cpp" }
+   files { "source/*.def" }
+   files { "source/*.rc" }
+      
+   filter "configurations:Debug"
+      defines "DEBUG"
+      symbols "On"
+
+   filter "configurations:Release"
+      defines "NDEBUG"
+      optimize "On"
+
+project "d3d9-wrapper"
