b2924f3006f8d1efa83a6ab6653e8e962cf40b1a
ThirteenAG <thirteenag@gmail.com>
2022-09-19 11:26:34 +0300

    crash fix

--- source/iathook.h
+++ source/iathook.h
@@ -4,9 +4,6 @@
 #include <windows.h>
 #include <stdint.h>
 
-/*
- * simple iathook, unreleased version
- */
 #ifdef __cplusplus
 namespace Iat_hook
 {
@@ -14,15 +11,44 @@ namespace Iat_hook
     void** find_iat_func(const char* function, HMODULE hModule, const char* chModule, const DWORD ordinal)
     {
         if (!hModule)
-            hModule = GetModuleHandle(0);
+            hModule = GetModuleHandle(nullptr);
+        
+        const DWORD_PTR instance = reinterpret_cast<DWORD_PTR>(GetModuleHandle(nullptr));
+        const PIMAGE_NT_HEADERS ntHeader = reinterpret_cast<PIMAGE_NT_HEADERS>(instance + reinterpret_cast<PIMAGE_DOS_HEADER>(instance)->e_lfanew);
+        PIMAGE_IMPORT_DESCRIPTOR pImports = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(instance + ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
 
-        PIMAGE_DOS_HEADER img_dos_headers = (PIMAGE_DOS_HEADER)hModule;
-        PIMAGE_NT_HEADERS img_nt_headers = (PIMAGE_NT_HEADERS)((BYTE*)img_dos_headers + img_dos_headers->e_lfanew);
-        PIMAGE_IMPORT_DESCRIPTOR img_import_desc = (PIMAGE_IMPORT_DESCRIPTOR)((BYTE*)img_dos_headers + img_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
-        //if (img_dos_headers->e_magic != IMAGE_DOS_SIGNATURE)
-            //printf("ERROR: e_magic is not a valid DOS signature\n");
+        for (; pImports->Name != 0; pImports++)
+        {
+            auto mod_name = reinterpret_cast<const char*>((size_t*)(pImports->Name + (size_t)hModule));
+            if (_stricmp(reinterpret_cast<const char*>(instance + pImports->Name), mod_name) == 0)
+            {
+                if (pImports->OriginalFirstThunk != 0)
+                {
+                    const PIMAGE_THUNK_DATA pThunk = reinterpret_cast<PIMAGE_THUNK_DATA>(instance + pImports->OriginalFirstThunk);
+                    for (ptrdiff_t j = 0; pThunk[j].u1.AddressOfData != 0; j++)
+                    {
+                        if (strcmp(reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(instance + pThunk[j].u1.AddressOfData)->Name, function) == 0)
+                        {
+                            void** pAddress = reinterpret_cast<void**>(instance + pImports->FirstThunk) + j;
+                            return pAddress;
+                        }
+                    }
+                }
+                else
+                {
+                    void** pFunctions = reinterpret_cast<void**>(instance + pImports->FirstThunk);
+                    for (ptrdiff_t j = 0; pFunctions[j] != nullptr; j++)
+                    {
+                        if (pFunctions[j] == function)
+                        {
+                            return &pFunctions[j];
+                        }
+                    }
+                }
+            }
+        }
 
-        for (IMAGE_IMPORT_DESCRIPTOR* iid = img_import_desc; iid->Name != 0; iid++) {
+        for (IMAGE_IMPORT_DESCRIPTOR* iid = pImports; iid->Name != 0; iid++) {
             if (chModule != NULL)
             {
                 char* mod_name = (char*)((size_t*)(iid->Name + (size_t)hModule));
@@ -34,18 +60,17 @@ namespace Iat_hook
                 char* mod_func_name = (char*)(mod_func_ptr_ord + (size_t)hModule + 2);
                 const intptr_t nmod_func_name = (intptr_t)mod_func_name;
                 if (nmod_func_name >= 0) {
-                    //printf("%s %s\n", mod_name, mod_func_name);
                     if (function != NULL && !lstrcmpA(function, mod_func_name))
                         return func_idx + (void**)(iid->FirstThunk + (size_t)hModule);
                 }
                 else if (IMAGE_SNAP_BY_ORDINAL(mod_func_ptr_ord))
                 {
-                    //printf("%s @%u\n", mod_name, IMAGE_ORDINAL(mod_func_ptr_ord));
                     if (chModule != NULL && ordinal != 0 && (ordinal == IMAGE_ORDINAL(mod_func_ptr_ord)))
                         return func_idx + (void**)(iid->FirstThunk + (size_t)hModule);
                 }
             }
         }
+
         return 0;
     }
 
